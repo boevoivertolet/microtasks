8f672a1a22c6a66ac361a0891f9630137b4153b7
boevoiverolet <bjha78rus@yandex.ru>
2022-04-06 14:01:03 +0300

    restore files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./Header";
-import {Week} from "./Week";
+
 
 
 function App() {
     return (
         < div className="App">
-            <Header/>
-            <Week/>
+
         </div>
     );
 
