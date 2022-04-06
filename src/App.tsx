a033aa9ef37235d5ae644fa4a5bc78f6ee2551e8
boevoiverolet <bjha78rus@yandex.ru>
2022-04-06 16:13:19 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import {NewComponent} from "./components/NewComponent";
 
 
 
 function App() {
     return (
         < div className="App">
-
+    <NewComponent students='Sahsa'/>
         </div>
     );
 
---
+++ src/components/NewComponent.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+type NewComponentType = {
+    students: any
+}
+
+
+
+export const NewComponent = (props: NewComponentType) => {
+    return (
+        <div>qqqqqqqqqqqqqqqqqqqaqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq</div>
+    );
+}
