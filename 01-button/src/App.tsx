17bc79d7e050e56b81422a737831974303cfe82f
boevoiverolet <bjha78rus@yandex.ru>
2022-04-07 14:46:15 +0300

    create universal button

--- 01-button/src/App.tsx
+++ 01-button/src/App.tsx
@@ -1,38 +1,25 @@
-import React, {MouseEvent} from 'react';
+import React from 'react';
 import './App.css';
+import {Button} from "./components/Button";
 
 
 function App() {
-    /*const myFirstSub =(event: MouseEvent<HTMLButtonElement>) => {
-        console.log('Hello! i am Vasya')
+    const Button1Foo = (name: string, age: number, adress: string) => {
+        console.log(name, age, adress)
     }
-    const mySecondSub =(event: MouseEvent<HTMLButtonElement>)=> {
-        console.log('Hello! i am  Ivan')
-    }*/
-
-     const onCliCkHandler =(name: string)=>{
-         console.log(name)
-     }
-
-/*    const foo1 = () => {
-        console.log('100200')
-// Ничего не передавать, но в консоли должно показаться 100200
+    const Button2Foo = (name: string, age: number, adress: string) => {
+        console.log(name, age, adress)
+    }
+    const Button3Foo = (text: string) => {
+        console.log(text)
     }
 
-    const foo2 = (num: number) => {
-        console.log(num)
-        // передать 100200
-    }*/
 
     return (
-        <div className='App'>
-             {/*  <button onClick={(event) =>{console.log('Hello!')}}>Say Hello 1</button>*/}
-            <button onClick={(event)=>onCliCkHandler('Vasya')}>MyYouTubeChanel-1</button>
-            <button onClick={(event)=>onCliCkHandler( 'Ivan')}>MyYouTubeChanel-2</button>
-           {/* <button onClick={() =>onCliCkHandler( 'Ivan')}>MyYouTubeChanel-2</button>*/}
-
-         {/*   <button onClick={foo1}>1</button>
-            <button onClick={() => foo2(100200)}>2</button>*/}
+        <div className={'App'}>
+            <Button name={'MyYouTubeChanel-1'} callBack={() => Button1Foo('Im Sasha', 34, 'Spb')}/>
+            <Button name={'MyYouTubeChanel-2'} callBack={() => Button2Foo('Im Ulya', 32, 'Minsk')}/>
+            <Button name={'Stupid button'} callBack={() => Button3Foo('Im Stupid button =(')}/>
         </div>
 
     )
