1e827a392c01d7d314d42e6d287db13a617475a5
boevoivertolet <bjha78rus@yandex.ru>
2022-05-01 20:53:57 +0300

    refactoring code. create button & input completed

--- 01-useState/src/App.tsx
+++ 01-useState/src/App.tsx
@@ -1,24 +1,23 @@
-import React,{useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
-
 
 
 function App() {
 // let a =1
-    let[a, setA]=useState(1)
+    let [a, setA] = useState(1)
 
     const onClickHandler = () => {
         setA(++a)
         console.log(a)
     }
     const dropToZero = () => {
-        setA(a=0)
+        setA(a = 0)
         console.log(a)
 
     }
 
     return (
-        <div className={"App"}>
+        <div className={'App'}>
             <h1>{a}</h1>
             <button onClick={onClickHandler}>+1</button>
             <button onClick={dropToZero}>0</button>
